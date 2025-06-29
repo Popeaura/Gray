5edda7bde5bd47133937050059b2b2a39251678a
popeaura <popeahura@gmail.com>
2025-06-29 17:45:20 +0300

    I added my modifed folder for my project

--- Mysite/Mysite/views.py
+++ Mysite/Mysite/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 def homepage(request):
    # return HttpResponse ("Good Morning Africa! I'm Home")
+   return render (request, 'home.html')
 
 def about(request):
     # return HttpResponse ("My About Page.") 
+    return render(request, 'about.html')
